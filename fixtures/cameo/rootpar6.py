01fba9747b895c2f754f58521089fe321628eca6
ari24-cb24 <bob.myandroid@gmail.com>
2025-01-22 16:35:02 +0100

    feat: Added root par 6 8ch mode fixture

---
+++ fixtures/cameo/rootpar6.py
@@ -0,0 +1,28 @@
+from fixtures.basefixture import BaseFixture, ColorFixture, StroboFixture
+
+
+class RootPar6(BaseFixture, ColorFixture, StroboFixture):
+    def __init__(self, rawChannel: int | None = None):
+        """
+        8ch mode
+        0 - dimmer
+        1 - strobe
+        2 - red
+        3 - green
+        4 - blue
+        5 - white
+        6 - amber
+        7 - uv
+        """
+        super().__init__(8, rawChannel)
+        self._initializeColorChannels(2, 3, 4, 0)
+        self._initializeStroboChannels(1)
+
+    def setWhite(self, value: int):
+        self.setValue(5, value)
+
+    def setAmber(self, value: int):
+        self.setValue(6, value)
+
+    def setUV(self, value: int):
+        self.setValue(7, value)
